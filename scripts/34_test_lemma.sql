ea13510837524f90c9446ba8eb0228a018969965
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-01-25 13:27:44 +0000

    adds first test

---
+++ scripts/34_test_lemma.sql
@@ -0,0 +1,29 @@
+CREATE TEMP TABLE _vars(key TEXT, value INTEGER);
+CREATE TEMP TABLE _res(key TEXT, value INTEGER);
+
+-- test whether all lemmata from RBE are included in the final lemma table
+INSERT INTO _vars VALUES('total_rbe_lemmata_inside_lemma_table',
+	(SELECT
+		COUNT(*)
+	FROM rbe_lemma r
+	WHERE
+		(
+			SELECT
+				COUNT(*)
+			FROM lemma l
+			WHERE r.lemma_with_stress = l.name_stressed AND r.pos = l.pos
+		) > 0)
+);
+
+INSERT INTO _vars VALUES('total_rbe_lemmata',
+	(SELECT
+		COUNT(*)
+	FROM rbe_lemma)
+);
+
+INSERT INTO _res VALUES('missing_rbe_lemmata', (SELECT value FROM _vars WHERE key = 'total_rbe_lemmata') - (SELECT value FROM _vars WHERE key = 'total_rbe_lemmata_inside_lemma_table'));
+
+SELECT * FROM _res;
+
+DROP TABLE _vars;
+DROP TABLE _res;
