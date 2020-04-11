99e87136fab174777645a9fe24789ae50cddc64b
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-04-11 20:16:13 +0100

    adds new lemma stress paradigms

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-To build the artifact, run:
+To build the artifact (without Slovnik), run:
 ```
-./build --help
+./build --no-slovnik
 ```
 This repository contains code that produces a dictionary of 200 000 Bulgarian lemmata and 2 million wordforms along with their stress patterns, morphosyntactic properties and pronunciation. It is meant to be used as a dictionary to assist with translating or writing metric and rhymed poetry in Bulgarian. It is based on previous Bulgarian linguistic resources such as Slovnik, RBE, Rechko and Murdarov's dictionary.
 
-The build script requires a Linux distribution, although the resulting artifact is a platform-independent SQLite file. Recommended RAM > 2GB. The script uses as many cores as possible when parallelization is feasible.
+The build script requires a Linux distribution, although the resulting artifact is a platform-independent SQLite file. Recommended RAM > 2.5GB. The script uses as many cores as possible when parallelization is feasible.
 
-For best results mount /tmp to main memory as tmpfs or ramfs. Build time is 2 minutes on a quad-core i7 @ 3.4 GHz.
+For best results mount /tmp to main memory as tmpfs or ramfs. Build time is 2 minutes 30 seconds on a quad-core i7 @ 3.4 GHz.
 
 All wordforms in the database are annotated using the BulTreeBank morphosyntactic tagset. To learn more about it, please refer to the technical report inside resources/.
 The Slovnik dictionary, however, is encrypted as it is not intended for public use. Add option --no-slovnik to skip augmenting Slovnik into the final artifact. If you want to gain access to Slovnik, contact a BTB member at http://bultreebank.org. Afterwards, I will provide you with the password to decrypt Slovnik.
--- pipeline/62_add_stress_to_lemma.sql
+++ pipeline/62_add_stress_to_lemma.sql
@@ -10,4 +10,70 @@ UPDATE lemma
 SET lemma_stressed = stress_syllable(lemma_stressed, num_syllables)
 WHERE source = 'murdarov' AND num_stresses = 0;
 
+-- here we use the fact thah some suffixes are very likely (or not) to be stressed, especially as heuristic for disyllabic words
+-- always stress first syllable of disyllabic adjectives ending in -ов
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, 1)
+WHERE num_syllables = 2 AND num_stresses = 0 AND lemma LIKE '%ов' AND pos = 'A';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, 1)
+WHERE num_syllables = 2 AND num_stresses = 0 AND lemma LIKE '%ис' AND pos LIKE 'Nc%';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%ок' AND lemma_id not in (84346);
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, 1)
+WHERE num_syllables = 2 AND num_stresses = 0 AND lemma LIKE '%ел' AND lemma_id not in (36621, 119194);
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, 1)
+WHERE num_syllables = 2 AND num_stresses = 0 AND lemma LIKE '%ер';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%вед' AND pos = 'Ncm';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%въд' ;
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%яд';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%ар';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%рък' AND lemma NOT LIKE '%ярък' AND pos = 'A';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%ач';
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables - 1)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%тел' AND pos != 'Np' AND lemma_id NOT IN (31436, 35036, 35790);
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%еж' AND lemma_id NOT IN (33067);
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables - 2)
+WHERE num_syllables = 3 AND num_stresses = 0 AND lemma LIKE '%ница' AND lemma_id NOT IN (14735, 18621, 19087, 21069, 21097, 22700);
+
+UPDATE lemma
+SET lemma_stressed = stress_syllable(lemma, num_syllables - 2)
+WHERE num_syllables > 0 AND num_stresses = 0 AND lemma LIKE '%нича' AND lemma_id NOT IN (96881, 102265, 100420, 100428, 180802);
+
+-- TODO: 70% of Ncm is 01
+-- TODO: 85% of Ncf is 10
+-- TODO: При повече от 84% от глаголите ударението е пенултимно (ходя, чакам), а при останалите оксинонно (чета, благодаря)
+
 END TRANSACTION;
