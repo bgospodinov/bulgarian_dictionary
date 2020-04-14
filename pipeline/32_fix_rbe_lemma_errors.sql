fadf38726f567095d89cf15e4a8068382aca3988
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-04-14 20:10:16 +0100

    fixes test failure

--- pipeline/32_fix_rbe_lemma_errors.sql
+++ pipeline/32_fix_rbe_lemma_errors.sql
@@ -9,20 +9,26 @@ UPDATE rbe_lemma SET lemma_with_stress = 'петдесе`ти' WHERE lemma_with_
 UPDATE rbe_lemma SET pos = 'Ncm' WHERE lemma IN ('ден', 'двор', 'пол', 'ом', 'оджак', 'драмкръжок') AND pos != 'Ncm';
 UPDATE rbe_lemma SET lemma = 'Бабинден', lemma_with_stress = 'Ба`бинден' WHERE lemma = 'бабинден' AND pos = 'Ncm';
 
--- separates words that have two possibl stresses
+-- separates words that have two possiblе stresses
 UPDATE rbe_lemma SET lemma_with_stress = 'боклу`ча' WHERE lemma_with_stress = 'боклу`ча`' AND pos = 'V';
 UPDATE rbe_lemma SET lemma_with_stress = 'бла`жа' WHERE lemma_with_stress = 'бла`жа`' AND pos = 'V';
 UPDATE rbe_lemma SET lemma_with_stress = 'ви`ся' WHERE lemma_with_stress = 'ви`ся`' AND pos = 'V';
 UPDATE rbe_lemma SET lemma_with_stress = 'вре`дя' WHERE lemma_with_stress = 'вре`дя`' AND pos = 'V';
 UPDATE rbe_lemma SET lemma_with_stress = 'начого`ля' WHERE lemma_with_stress = 'начого`ля`' AND pos = 'V';
 UPDATE rbe_lemma SET lemma_with_stress = 'пра`ся' WHERE lemma_with_stress = 'пра`ся`' AND pos = 'V';
+UPDATE rbe_lemma SET lemma_with_stress = 'позакръ`гля' WHERE lemma_with_stress = 'позакръ`гля`' AND pos = 'V';
+UPDATE rbe_lemma SET lemma_with_stress = 'вкле`щя' WHERE lemma_with_stress = 'вкле`щя`' AND pos = 'V';
+UPDATE rbe_lemma SET lemma_with_stress = 'наока`ча' WHERE lemma_with_stress = 'наока`ча`' AND pos = 'V';
 
 INSERT INTO rbe_lemma (lemma, lemma_with_stress, pos) VALUES
     ('боклуча', 'боклуча`', 'V'),
     ('вися', 'вися`', 'V'),
     ('вредя', 'вредя`', 'V'),
     ('начоголя', 'начоголя`', 'V'),
-    ('прася', 'прася`', 'V')
+    ('прася', 'прася`', 'V'),
+    ('позакръгля', 'позакръгля`', 'V'),
+    ('вклещя', 'вклещя`', 'V'),
+    ('наокача', 'наокача`', 'V')
 ;
 
 END TRANSACTION;
