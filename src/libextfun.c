584ceb1fdf3d57d1f0b05d377a796d30a601624d
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-04-24 23:51:21 +0100

    removes useless header

--- src/libextfun.c
+++ src/libextfun.c
@@ -6,50 +6,11 @@
 #include "../inc/string_aux.h"
 SQLITE_EXTENSION_INIT1
 
-static void sqlite_is_capitalized(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_is_lemma(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_is_vocal(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_count_syllables(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_rechko_tag(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_stress_syllable(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_replace_last_stress(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_remove_last_char(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_find_nth_stressed_syllable(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_find_nth_stressed_syllable_rev(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_diminutive_to_base(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_accent_model(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_sonority_model(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_reverse(sqlite3_context *context, int argc, sqlite3_value **argv);
-static void sqlite_pronounce(sqlite3_context *context, int argc, sqlite3_value **argv);
-
-int sqlite3_extfun_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
-	setlocale(LC_ALL, "");
-	int rc = SQLITE_OK;
-	SQLITE_EXTENSION_INIT2(pApi);
-	// registering all custom sqlite functions
-	sqlite3_create_function(db, "is_capitalized", 1, SQLITE_UTF8, NULL, &sqlite_is_capitalized, NULL, NULL);
-	sqlite3_create_function(db, "is_lemma", 3, SQLITE_UTF8, NULL, &sqlite_is_lemma, NULL, NULL);
-	sqlite3_create_function(db, "is_vocal", 1, SQLITE_UTF8, NULL, &sqlite_is_vocal, NULL, NULL);
-	sqlite3_create_function(db, "count_syllables", 1, SQLITE_UTF8, NULL, &sqlite_count_syllables, NULL, NULL);
-	sqlite3_create_function(db, "rechko_tag", 3, SQLITE_UTF8, NULL, &sqlite_rechko_tag, NULL, NULL);
-	sqlite3_create_function(db, "stress_syllable", 2, SQLITE_UTF8, NULL, &sqlite_stress_syllable, NULL, NULL);
-	sqlite3_create_function(db, "replace_last_stress", 2, SQLITE_UTF8, NULL, &sqlite_replace_last_stress, NULL, NULL);
-	sqlite3_create_function(db, "remove_last_char", 2, SQLITE_UTF8, NULL, &sqlite_remove_last_char, NULL, NULL);
-	sqlite3_create_function(db, "find_nth_stressed_syllable", 2, SQLITE_UTF8, NULL, &sqlite_find_nth_stressed_syllable, NULL, NULL);
-	sqlite3_create_function(db, "find_nth_stressed_syllable_rev", 2, SQLITE_UTF8, NULL, &sqlite_find_nth_stressed_syllable_rev, NULL, NULL);
-	sqlite3_create_function(db, "diminutive_to_base", 1, SQLITE_UTF8, NULL, &sqlite_diminutive_to_base, NULL, NULL);
-	sqlite3_create_function(db, "accent_model", 1, SQLITE_UTF8, NULL, &sqlite_accent_model, NULL, NULL);
-	sqlite3_create_function(db, "sonority_model", 1, SQLITE_UTF8, NULL, &sqlite_sonority_model, NULL, NULL);
-	sqlite3_create_function(db, "reverse", 1, SQLITE_UTF8, NULL, &sqlite_reverse, NULL, NULL);
-	sqlite3_create_function(db, "pronounce", 1, SQLITE_UTF8, NULL, &sqlite_pronounce, NULL, NULL);
-	return rc;
-}
-
 static void sqlite_is_lemma(sqlite3_context *context, int argc, sqlite3_value **argv) {
 	if (argc == 3) {
-		const char *word = sqlite3_value_text(argv[0]);
-		const char *lemma = sqlite3_value_text(argv[1]);
-		const char *tag = sqlite3_value_text(argv[2]);
+		const char * word = sqlite3_value_text(argv[0]);
+		const char * lemma = sqlite3_value_text(argv[1]);
+		const char * tag = sqlite3_value_text(argv[2]);
 		if (word && lemma && tag) {
 			int result = is_lemma(word, lemma, tag);
 			sqlite3_result_int(context, result);
@@ -264,4 +225,27 @@ static void sqlite_is_capitalized(sqlite3_context *context, int argc, sqlite3_va
 	}
 
 	sqlite3_result_null(context);
+}
+
+int sqlite3_extfun_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
+	setlocale(LC_ALL, "");
+	int rc = SQLITE_OK;
+	SQLITE_EXTENSION_INIT2(pApi);
+	// registering all custom sqlite functions
+	sqlite3_create_function(db, "is_capitalized", 1, SQLITE_UTF8, NULL, &sqlite_is_capitalized, NULL, NULL);
+	sqlite3_create_function(db, "is_lemma", 3, SQLITE_UTF8, NULL, &sqlite_is_lemma, NULL, NULL);
+	sqlite3_create_function(db, "is_vocal", 1, SQLITE_UTF8, NULL, &sqlite_is_vocal, NULL, NULL);
+	sqlite3_create_function(db, "count_syllables", 1, SQLITE_UTF8, NULL, &sqlite_count_syllables, NULL, NULL);
+	sqlite3_create_function(db, "rechko_tag", 3, SQLITE_UTF8, NULL, &sqlite_rechko_tag, NULL, NULL);
+	sqlite3_create_function(db, "stress_syllable", 2, SQLITE_UTF8, NULL, &sqlite_stress_syllable, NULL, NULL);
+	sqlite3_create_function(db, "replace_last_stress", 2, SQLITE_UTF8, NULL, &sqlite_replace_last_stress, NULL, NULL);
+	sqlite3_create_function(db, "remove_last_char", 2, SQLITE_UTF8, NULL, &sqlite_remove_last_char, NULL, NULL);
+	sqlite3_create_function(db, "find_nth_stressed_syllable", 2, SQLITE_UTF8, NULL, &sqlite_find_nth_stressed_syllable, NULL, NULL);
+	sqlite3_create_function(db, "find_nth_stressed_syllable_rev", 2, SQLITE_UTF8, NULL, &sqlite_find_nth_stressed_syllable_rev, NULL, NULL);
+	sqlite3_create_function(db, "diminutive_to_base", 1, SQLITE_UTF8, NULL, &sqlite_diminutive_to_base, NULL, NULL);
+	sqlite3_create_function(db, "accent_model", 1, SQLITE_UTF8, NULL, &sqlite_accent_model, NULL, NULL);
+	sqlite3_create_function(db, "sonority_model", 1, SQLITE_UTF8, NULL, &sqlite_sonority_model, NULL, NULL);
+	sqlite3_create_function(db, "reverse", 1, SQLITE_UTF8, NULL, &sqlite_reverse, NULL, NULL);
+	sqlite3_create_function(db, "pronounce", 1, SQLITE_UTF8, NULL, &sqlite_pronounce, NULL, NULL);
+	return rc;
 }
