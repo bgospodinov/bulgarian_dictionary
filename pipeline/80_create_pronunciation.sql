a939ec3a46787db50db51f927a1ac5b40008be1b
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-04-24 11:42:25 +0100

    adds stressed pronunciation

--- pipeline/80_create_pronunciation.sql
+++ pipeline/80_create_pronunciation.sql
@@ -4,6 +4,7 @@ CREATE TABLE pronunciation (
   pronunciation_id INTEGER PRIMARY KEY,
   wordform_id INT(11),
   pronunciation TEXT,
+  pronunciation_stressed TEXT,
   is_normative INT DEFAULT 1,
   FOREIGN KEY(wordform_id) REFERENCES wordform(wordform_id)
   ON DELETE CASCADE
--- pipeline/81_add_pronunciation.sql
+++ pipeline/81_add_pronunciation.sql
@@ -1,19 +1,24 @@
 BEGIN TRANSACTION;
 
-INSERT INTO pronunciation (wordform_id, pronunciation)
-SELECT wordform_id, pronounce(wordform) FROM wordform;
+INSERT INTO pronunciation (wordform_id, pronunciation, pronunciation_stressed)
+SELECT wordform_id, pronounce(wordform), pronounce(wordform_stressed) FROM wordform;
 
 -- deal with masculine nouns with short definite article
 CREATE TEMP TABLE Ncmsh AS
-SELECT p.wordform_id, p.pronunciation FROM pronunciation p
+SELECT p.wordform_id, p.pronunciation, p.pronunciation_stressed FROM pronunciation p
 LEFT JOIN wordform w ON w.wordform_id = p.wordform_id
 WHERE w.tag = 'Ncmsh' AND pronunciation LIKE '%а';
 
-INSERT INTO pronunciation (wordform_id, pronunciation, is_normative)
+INSERT INTO pronunciation (wordform_id, pronunciation, pronunciation_stressed, is_normative)
 SELECT *, 0 FROM Ncmsh;
 
 UPDATE pronunciation
-SET pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 1) || 'ъ'
+SET
+    pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 1) || 'ъ',
+    pronunciation_stressed = CASE WHEN pronunciation_stressed LIKE '%`'
+            THEN SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 2) || 'ъ`'
+            ELSE SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 1) || 'ъ'
+            END
 WHERE wordform_id IN (
     SELECT wordform_id FROM Ncmsh
 ) AND is_normative = 1;
@@ -22,7 +27,7 @@ WHERE wordform_id IN (
 -- in Bulgarian conjugation is determined by the last vowel of the 3rd person singular present tense wordform of the verb
 -- 1st conjugation ends in 'е' and 2nd conjugation in 'и'
 CREATE TEMP TABLE SpecialVerbs AS
-SELECT p.wordform_id, p.pronunciation FROM pronunciation p
+SELECT p.wordform_id, p.pronunciation, p.pronunciation_stressed FROM pronunciation p
 LEFT JOIN wordform w ON w.wordform_id = p.wordform_id
 WHERE w.lemma_id IN
 (
@@ -30,17 +35,27 @@ WHERE w.lemma_id IN
 )
 AND (w.tag LIKE '%r1s' OR w.tag LIKE '%r3p');
 
-INSERT INTO pronunciation (wordform_id, pronunciation, is_normative)
+INSERT INTO pronunciation (wordform_id, pronunciation, pronunciation_stressed, is_normative)
 SELECT *, 0 FROM SpecialVerbs;
 
 UPDATE pronunciation
-SET pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 1) || 'ъ'
+SET
+    pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 1) || 'ъ',
+    pronunciation_stressed = CASE WHEN pronunciation_stressed LIKE '%`'
+            THEN SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 2) || 'ъ`'
+            ELSE SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 1) || 'ъ'
+            END
 WHERE wordform_id IN (
     SELECT wordform_id FROM SpecialVerbs WHERE pronunciation LIKE '%а'
 ) AND is_normative = 1;
 
 UPDATE pronunciation
-SET pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 2) || 'ът'
+SET
+    pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 2) || 'ът',
+    pronunciation_stressed = CASE WHEN pronunciation_stressed LIKE '%`т'
+            THEN SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 3) || 'ъ`т'
+            ELSE SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 2) || 'ът'
+            END
 WHERE wordform_id IN (
     SELECT wordform_id FROM SpecialVerbs WHERE pronunciation LIKE '%ат'
 ) AND is_normative = 1;
