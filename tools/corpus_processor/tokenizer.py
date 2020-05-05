7678ac75bf6453aef633efcc28cd31d6f66e0239
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-05-05 15:03:39 +0100

    renames to corpus_processor

--- tools/corpus_processor/tokenizer.py
+++ tools/corpus_processor/tokenizer.py
@@ -3,9 +3,9 @@ import re
 
 def tokenize(text):
     """Simple Cyrillic tokenization."""
-    return re.split(r'[A-Za-z\W_\d]*[\s/|]+[A-Za-z\W_]*', text)
+    return re.split(r'[\W_\d]*[\s/|]+[\W_]*', text)
 
 
 def filter_non_cyrillic(tokens):
-    r_legal_symbols = re.compile(r'[\u0410-\u044F\-\'–—,°’:.0-9]+')
+    r_legal_symbols = re.compile(r'[\u0410-\u044F\-\'–—,°’:.\d]+')
     return list(filter(lambda t: r_legal_symbols.fullmatch(t), tokens))
