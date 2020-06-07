247129e6f4629002caccbf34eeae248c1a15b09f
Bogomil Gospodinov <bgospodinov@protonmail.com>
2020-06-07 13:49:25 +0100

    fixes pronunciation of nouns with full definite article

--- pipeline/81_add_pronunciation.sql
+++ pipeline/81_add_pronunciation.sql
@@ -9,19 +9,15 @@ SELECT p.wordform_id, p.pronunciation, p.pronunciation_stressed FROM pronunciati
 LEFT JOIN wordform w ON w.wordform_id = p.wordform_id
 WHERE w.tag = 'Ncmsh' AND pronunciation LIKE '%а';
 
-INSERT INTO pronunciation (wordform_id, pronunciation, pronunciation_stressed, is_normative)
-SELECT *, 0 FROM Ncmsh;
+-- deal with masculine nouns with full definite article
+CREATE TEMP TABLE Ncmsf AS
+SELECT p.wordform_id, p.pronunciation, p.pronunciation_stressed FROM pronunciation p
+LEFT JOIN wordform w ON w.wordform_id = p.wordform_id
+WHERE w.tag = 'Ncmsf' AND pronunciation LIKE '%йат';
 
-UPDATE pronunciation
-SET
-    pronunciation = SUBSTR(pronunciation, 1, LENGTH(pronunciation) - 1) || 'ъ',
-    pronunciation_stressed = CASE WHEN pronunciation_stressed LIKE '%`'
-            THEN SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 2) || 'ъ`'
-            ELSE SUBSTR(pronunciation_stressed, 1, LENGTH(pronunciation_stressed) - 1) || 'ъ'
-            END
-WHERE wordform_id IN (
-    SELECT wordform_id FROM Ncmsh
-) AND is_normative = 1;
+INSERT INTO pronunciation (wordform_id, pronunciation, pronunciation_stressed, is_normative)
+SELECT *, 0 FROM Ncmsh
+UNION SELECT *, 0 FROM Ncmsf;
 
 -- deal with 1st person singular verbs and 3rd person plural verbs of 1st and 2nd conjugation
 -- in Bulgarian conjugation is determined by the last vowel of the 3rd person singular present tense wordform of the verb
@@ -47,6 +43,7 @@ SET
             END
 WHERE wordform_id IN (
     SELECT wordform_id FROM SpecialVerbs WHERE pronunciation LIKE '%а'
+    UNION SELECT wordform_id FROM Ncmsh
 ) AND is_normative = 1;
 
 UPDATE pronunciation
@@ -58,6 +55,7 @@ SET
             END
 WHERE wordform_id IN (
     SELECT wordform_id FROM SpecialVerbs WHERE pronunciation LIKE '%ат'
+    UNION SELECT wordform_id FROM Ncmsf
 ) AND is_normative = 1;
 
 -- TODO: nouns ending in -ст
